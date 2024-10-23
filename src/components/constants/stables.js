88af2393879060c9e38e625699edfc68a399c547
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2024-10-23 09:48:36 +0300

    Updated

--- src/components/constants/stables.js
+++ src/components/constants/stables.js
@@ -1,5 +1,5 @@
-const CLOUDINARY_BASE_URL = process.env.REACT_APP_CLOUDINARY_BASE_URL;
-const BLOG_APP_FOLDER = process.env.REACT_APP_CLOUDINARY_BLOG_APP_FOLDER;
+const CLOUDINARY_BASE_URL = "https://res.cloudinary.com/dqqynqpwp/image/upload";
+const BLOG_APP_FOLDER = "v1729611956/blog-app";
 
 const UPLOAD_FOLDER_BASE_URL = `${CLOUDINARY_BASE_URL}/${BLOG_APP_FOLDER}/`;
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
